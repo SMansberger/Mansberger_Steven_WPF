5911fb00d2f4680aa8c5ad9fb5cb44b4666ccd5e
SMansberger <Smansbe2@fullsail.edu>
2013-08-16 02:52:31 -0400

    Finalized Expressions_Wacky

--- Mansberger_Steven_WPF/Expressions_Wacky/js/script.js
+++ Mansberger_Steven_WPF/Expressions_Wacky/js/script.js
@@ -1,1 +1,29 @@
-alert("This is in the js file");
+/* Steven Mansberger
+08.15.2013
+Expression_Wacky */
+
+
+alert("Welcome to the Random Sentence Creator!")
+
+var randomWords = [];
+
+var noun = prompt("Please enter a noun: ");
+randomWords[0] = noun;
+
+var verb = prompt("Please enter a verb: ");
+randomWords[1] = verb;
+
+var directObj = prompt("Please enter another noun: ");
+randomWords[2] = directObj;
+
+alert("Creating your random sentence...");
+alert("Your sentence has been created! Press ok to continue!");
+
+var arrayOne = ["The " + noun + " will " + verb + " over the " + directObj + "." , 
+				"Today, twenty " + noun + " are going to " + verb + " behind the " + directObj + "." ,
+				"Our " + noun + " have to " + verb + " under the " + directObj] + ".";
+
+var randomNumber = Math.floor(Math.random() * arrayOne.length);
+var randomSelection = arrayOne[randomNumber];
+
+alert(randomSelection);
