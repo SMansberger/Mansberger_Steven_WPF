e377631363b3a78303cb4201587b52e7342b77f8
SMansberger <Smansbe2@fullsail.edu>
2013-08-22 03:24:39 -0400

    Completed playerDivsion. Going to being calculation section for determining game rank and games needed

--- Mansberger_Steven_WPF/Conditionals_Personal/js/script.js
+++ Mansberger_Steven_WPF/Conditionals_Personal/js/script.js
@@ -8,12 +8,15 @@ Each tier is further broken down into 5 divisions (V, IV, III, II, and I).
 To become considered for a pro team, you must make it to the highest division of the highest tier.
 Points are awareded for each game won. You must reach a total of 100 points in a division to place into the next division.
 Once you have earned 100 points to place, you must win the next 2 out of 3 games to advance. 
-Once you reach 100 points of the highest division of a tier, such as, Silver I, you must win 3 of the next 5 games to place into Gold V.*/
+Once you reach 100 points of the highest division of a tier, such as, Silver I, you must win 3 of the next 5 games to place into Gold V.
+We will assume you win 20 points per game, for the sake of Math =)*/
+
 var tier = 1;
 var division = 1;
 var gamesRequired = 1;
 alert("We are going to see how many games you must win to become a professional League of Legends Player!");
 
+//prompting user for tier placement. Taking into account some users may not capitalize letters and may abbreviate the "Platinum" tier name.
 var playerTier=prompt("Please enter your current placement tier (e.g. Silver, Gold, Platinum...): ", "Bronze");
 
 if(playerTier=="Bronze" || playerTier=="bronze"){ 
@@ -29,8 +32,18 @@ if(playerTier=="Bronze" || playerTier=="bronze"){
 }else{
 	alert("You did not enter a correct tier designation.");
 
+//prompting user for division placeent. Taking into account upper- and lower-case alternate spellings.
 var playerDivision=prompt("Next, please enter your current division within the " + playerTier + " tier.", "V");
 
 if(playerDivision=="V" || playerDivision=="v"){
 	division = 1;
-}else if
+}else if(playerDivision=="IV" || playerDivision=="iv"){
+	division =2;
+}else if(playerDivision=="III" || playerDivision=="iii"){
+	division =3;
+}else if(playerDivision=="II" || playerDivision=="ii"){
+	division =4;
+}else if(playerDivision=="I" || playerDivision=="i"){
+	division =5;
+}else{
+	alert("You did not enter a correct division designation.");
