3e47314debdf28075d87a32da70ea0dc68532599
SMansberger <Smansbe2@fullsail.edu>
2013-08-22 02:40:36 -0400

    Updated initial notes section to properly reflect ideology behind script

--- Mansberger_Steven_WPF/Conditionals_Personal/js/script.js
+++ Mansberger_Steven_WPF/Conditionals_Personal/js/script.js
@@ -3,8 +3,12 @@
 Conditionals_Personal */
 
 /*I am going to determine how many games I must win in order to become a professional League of Legends Player.
-Players are sorted into one of 6 tiers and each tier is further broken down into 5 divisions.
-To become considered for a pro team, you must make it to the highest division of the highest tier.*/
+Players are sorted into one of 6 tiers (bronze, gold, silver, platinum, diamond, and challenger). 
+Each tier is further broken down into 5 divisions (V, IV, III, II, and I).
+To become considered for a pro team, you must make it to the highest division of the highest tier.
+Points are awareded for each game won. You must reach a total of 100 points in a division to place into the next division.
+Once you have earned 100 points to place, you must win the next 2 out of 3 games to advance. 
+Once you reach 100 points of the highest division of a tier, such as, Silver I, you must win 3 of the next 5 games to place into Gold V.*/
 
 alert("We are going to see how many games you must win to become a professional League of Legends Player!");
 
