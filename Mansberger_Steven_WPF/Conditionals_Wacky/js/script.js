8767ae4231d8f7761f44dca990c9d9df0c08518e
SMansberger <Smansbe2@fullsail.edu>
2013-08-22 22:56:29 -0400

    Corrected values to properly represent adequate water intake (AI) per gender. Completed calculation. Error testing passes.

--- Mansberger_Steven_WPF/Conditionals_Wacky/js/script.js
+++ Mansberger_Steven_WPF/Conditionals_Wacky/js/script.js
@@ -7,7 +7,13 @@ it is suggested that men drink at least 3 litres of water a day and women drink 
 First, we will prompt the user whether they are male or female and then we will utilize ternaries to determine if the amount of water they drink is adequate
 For simplicity's sake, we are not going to decide if they drink too much water in a given day. Maybe in the future :) */
 
-var gender = prompt("I will let you know if you drink enough water! First, are you male or female?", neutral);
+var gender = prompt("I will let you know if you drink enough water! First, are you male or female?", "male");
 var water = prompt("How much water do you drink a day (in ounces)?", 0);
 
-if(gender==male
+if(gender=="male"){
+	(water>=101.4) ? console.log("You drink plenty of water! Keep it up!") : console.log("You should drink at least "+ (101.4 - water) + "oz more water per day!");
+}else if(gender=="female"){
+	(water>=74.4) ? console.log("You drink plenty of water! Keep it up!") : console.log("You should drink at least "+ (74.4 - water) + "oz more water per day!");
+}else{
+	console.log("Please answer the questions properly, stating your gender and then the amount of water you drink daily.");
+}
