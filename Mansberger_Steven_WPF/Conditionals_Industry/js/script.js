54a3ccace1b91ca68cc7d7ad2d5511f9d2bd7c5a
SMansberger <Smansbe2@fullsail.edu>
2013-08-22 22:15:22 -0400

    Created initial prompt for user media and storage. Beginning work on conditional statements and equations concerning storage space

--- Mansberger_Steven_WPF/Conditionals_Industry/js/script.js
+++ Mansberger_Steven_WPF/Conditionals_Industry/js/script.js
@@ -6,5 +6,10 @@ Conditional_Industry*/
 I belive the list will include items such as an .mp3 file, a .avi DVD file, or an .mkv file and the media will have a selection of a 3.5" floppy, a DVD, a Blu-ray disc ,a USB drive, and an external HDD*/
 
 alert("We are going to compare media sizes and the storage devices you can use to keep them.");
+alert("First, I need to know what kind of file you are trying to store.");
+var media = prompt("If you have an .mp3 file, please enter 1 \nIf you have an .avi movie file, please enter 2 \nIf you have an .mkv Matroska HD Movie File, please enter 3");
 
-var media = prompt("What kind of media are you trying to store the file on?", 
+alert("What kind of media are you trying to store the file on?");
+var storage = prompt("If you are trying to use a 3.5\" floppy, please enter 1 \nIf you are trying to use an 8GB USB drive, please enter 2 \nIf you are trying to use a 9.4GB DVD, please enter 3 \nIf you are trying to use a 25GB Blu-ray Disc, please enter 4");
+ 
+ 
