0c802d951a22acffba7b2c880e973c572c6f6e3b
SMansberger <Smansbe2@fullsail.edu>
2013-08-22 22:04:27 -0400

    Created initial notation and decided on a scenario for the code

--- Mansberger_Steven_WPF/Conditionals_Industry/js/script.js
+++ Mansberger_Steven_WPF/Conditionals_Industry/js/script.js
@@ -1,3 +1,10 @@
 /* Steven Mansberger
 08.20.2013
 Conditional_Industry*/
+
+/* I am going to create a prompt that will ask users about their preferred method of storing a selection of data on a certain set of media.
+I belive the list will include items such as an .mp3 file, a .avi DVD file, or an .mkv file and the media will have a selection of a 3.5" floppy, a DVD, a Blu-ray disc ,a USB drive, and an external HDD*/
+
+alert("We are going to compare media sizes and the storage devices you can use to keep them.");
+
+var media = prompt("What kind of media are you trying to store the file on?", 
