061bb5db83f7d332b17f70a81bb430c68fa7df05
SMansberger <Smansbe2@fullsail.edu>
2013-08-29 22:49:19 -0400

    Created calculation and established size of barrel

--- Mansberger_Steven_WPF/Functions_Personal/js/script.js
+++ Mansberger_Steven_WPF/Functions_Personal/js/script.js
@@ -2,3 +2,17 @@
 08.29.2013
 Functions_Personal */
 
+/* This will be a basic program to decide how many apples can fit in a semi-cylindrical basket with a height of 28.5 in, 
+a base with diameter of 24in, and a head with diameter of 40 in*/
+
+var apples = 1;
+
+var applesBasket = function(){
+	/*volume of a frustrum
+	v= (3.14(h))/3 * (R^2 + Rr + r^2)
+	*/
+	volumeBarrel = ((3.14 * 28.5) / 3) * ((40 * 40) + (40 * 24) + (24 * 24));
+	return volumeBarrel;
+}
+	
+console.log(volumeBarrel);
