515fd14a199c11713325d85f8880e4c13ef64841
SMansberger <Smansbe2@fullsail.edu>
2013-08-30 00:05:25 -0400

    Creating Date() function and parse command to determine exact amount of time from Jan 1, 1970 til this year's christmas and til today.

--- Mansberger_Steven_WPF/Functions_Wacky/js/script.js
+++ Mansberger_Steven_WPF/Functions_Wacky/js/script.js
@@ -1,3 +1,9 @@
 /* Steven Mansberger
 08.29.2013
 Functions_Wacky */
+
+/*This will be a very simple program to determine how long we have until christmas!*/
+
+  myDate=new Date(); 
+cmas=Date.parse("Dec 25, "+myDate.getFullYear());
+today=Date.parse(myDate);
