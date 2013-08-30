2c3b2c4d118f69d653f2008dc25868aab175a418
SMansberger <Smansbe2@fullsail.edu>
2013-08-29 23:05:20 -0400

    Completed equation and filled in average apple volume

--- Mansberger_Steven_WPF/Functions_Personal/js/script.js
+++ Mansberger_Steven_WPF/Functions_Personal/js/script.js
@@ -3,9 +3,11 @@
 Functions_Personal */
 
 /* This will be a basic program to decide how many apples can fit in a semi-cylindrical basket with a height of 28.5 in, 
-a base with diameter of 24in, and a head with diameter of 40 in*/
+a base with diameter of 24in, and a head with diameter of 40 in.
+Average apple volume = 4/3 * 3.14 * r^3*/
 
 var apples = 1;
+var volumeApple = 4 / 3 * 3.14 * 3 * 3 * 3;
 
 var applesBasket = function(){
 	/*volume of a frustrum
@@ -14,5 +16,7 @@ var applesBasket = function(){
 	volumeBarrel = ((3.14 * 28.5) / 3) * ((40 * 40) + (40 * 24) + (24 * 24));
 	return volumeBarrel;
 }
+
+volumeDifference = applesBasket() / volumeApple;
 	
-console.log(volumeBarrel);
+console.log ("The average apple barrel can hold " + volumeDifference + " apples.");
