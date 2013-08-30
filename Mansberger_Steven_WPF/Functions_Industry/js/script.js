2fcaf09fca303fd523fc4dba2d3d0610945787a0
SMansberger <Smansbe2@fullsail.edu>
2013-08-29 23:44:40 -0400

    Added ternary to satisfy conditions.

--- Mansberger_Steven_WPF/Functions_Industry/js/script.js
+++ Mansberger_Steven_WPF/Functions_Industry/js/script.js
@@ -3,7 +3,19 @@
 Function_Industry*/
 
 /*This will be a brief program used to determine the minimum number of commits to my GitHub in total for this class.*/
-
-var commitsAssignment = 20;
+/*var commitsAssignment = 20;
 var assignmentsWeek = 1;
 var weeks = 4;
+*/
+
+var numberWeeks = prompt("How many weeks will this class go for?", 4);
+
+var finalTotal = totalCommits(20, 1, numberWeeks);
+
+function totalCommits(c, a, w){
+	var numberCommits = c * a * w;
+	return numberCommits;
+}
+(numberWeeks>0) ? (console.log("You will have to commit code to your GitHub " + finalTotal + " times throughout this class.")) : console.log("Please enter a positive number of class weeks.");
+
+
--- Mansberger_Steven_WPF/Functions_Personal/js/script.js
+++ Mansberger_Steven_WPF/Functions_Personal/js/script.js
@@ -2,10 +2,13 @@
 08.29.2013
 Functions_Personal */
 
-/* This will be a basic program to decide how many apples can fit in a semi-cylindrical basket with a height of 28.5 in, 
+/* My grandfather owns an apple orchard.
+This will be a basic program to decide how many apples can fit in a semi-cylindrical basket with a height of 28.5 in, 
 a base with diameter of 24in, and a head with diameter of 40 in.
 Average apple volume = 4/3 * 3.14 * r^3*/
+
 var apples = prompt("How many apples are you trying to fit in the barrel?",1);
+
 var volumeApple = 4 / 3 * 3.14 * 3 * 3 * 3;
 
 var applesBasket = function(){
