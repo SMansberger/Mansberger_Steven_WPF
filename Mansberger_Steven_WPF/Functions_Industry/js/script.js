919935ce78ec8a236f8d0754021d144ae6d04063
SMansberger <Smansbe2@fullsail.edu>
2013-08-29 23:32:22 -0400

    Inital program idea and code layout

--- Mansberger_Steven_WPF/Functions_Industry/js/script.js
+++ Mansberger_Steven_WPF/Functions_Industry/js/script.js
@@ -2,3 +2,8 @@
 08.29.2013
 Function_Industry*/
 
+/*This will be a brief program used to determine the minimum number of commits to my GitHub in total for this class.*/
+
+var commitsAssignment = 20;
+var assignmentsWeek = 1;
+var weeks = 4;
